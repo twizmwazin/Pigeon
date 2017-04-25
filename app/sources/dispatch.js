39030358313b03fe7131e26928d9b89102c34821
Kevin Phoenix <twizmwazin@gmail.com>
2017-04-24 21:34:18 -0400

    Add backend for stocks

--- app/sources/dispatch.js
+++ app/sources/dispatch.js
@@ -7,6 +7,8 @@ let cron = require('node-cron');
 function run(db) {
   let weather = require('./weather');
   cron.schedule('*/5 * * * *', weather.update(db));
+  let stock = require('./stock');
+  cron.schedule('*/2 * * * *', stock.update(db));
 };
 
 module.exports = {run};
---
+++ app/sources/stock.js
@@ -0,0 +1,48 @@
+/*
+ * A source to get live stock data from alphavantage.co.
+ * Relevant data is stored in mongo, ready to be used by the front-end.
+ */
+let request = require('request');
+let config = require('config');
+let stockConf = config.get('stock');
+let token = stockConf.get('token');
+let symbols = stockConf.get('symbols');
+
+/**
+ * Updates all data for this module.
+ * @param {database} db The mongo database/
+ */
+function update(db) {
+  for (let i = 0; i < symbols.length; ++i) {
+    db.createCollection('stock', function(err, collection) {
+      collection.deleteOne({symbol: symbols[i]});
+      request(formatURL(symbols[i], token), function(err, response, body) {
+        let jbody = JSON.parse(body);
+        let d = new Date();
+        let date = (d.getYear() + 1900)
+          + '-' + (d.getMonth() < 10 ? '0' + d.getMonth() : d.getMonth())
+          + '-' + (d.getDay() < 10 ? '0' + d.getDay() : d.getDay());
+        collection.insertOne({
+          symbol: symbols[i],
+          updated: Date.now(),
+          open: jbody['Time Series (Daily)'][date]['1. open'],
+          close: jbody['Time Series (Daily)'][date]['4. close'],
+        });
+        console.log('Updated stocks successfully');
+      });
+    });
+  }
+};
+
+/**
+ * Formats URL for the API request
+ * @param  {string} symbol The stock symbol
+ * @param  {string} key    The API key
+ * @return {string}        The formatted URL
+ */
+function formatURL(symbol, key) {
+  return 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
+    + symbol + '&apikey=' + key;
+}
+
+module.exports = {update};
