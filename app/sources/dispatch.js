f13e04f6b595b87f2e7e883b54396a9b17b71380
Kevin Phoenix <twizmwazin@gmail.com>
2017-04-28 10:59:27 -0400

    Fix sources not automatically updating

--- app/sources/dispatch.js
+++ app/sources/dispatch.js
@@ -6,9 +6,13 @@ let cron = require('node-cron');
  */
 function run(db) {
   let weather = require('./weather');
-  cron.schedule('*/5 * * * *', weather.update(db));
+  cron.schedule('*/5 * * * *', function() {
+    weather.update(db);
+  });
   let stock = require('./stock');
-  cron.schedule('*/2 * * * *', stock.update(db));
+  cron.schedule('*/2 * * * *', function() {
+    stock.update(db);
+  });
 };
 
 module.exports = {run};
